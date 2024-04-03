eeaaf50bee884bd1abd2989da326f98659cabfa5
Muhammad Qaiser Majeed <mqaisermajeed@gmail.com>
2024-04-03 13:17:23 +0500

    Update Strings_in_Python.py
    The provided Python code performs a series of string and integer operations. Initially, it prints a multi-line string 'hello' followed by 'world' on a new line with indentation. Next, the code declares an integer variable number

--- Strings_in_Python.py
+++ Strings_in_Python.py
@@ -1,1 +1,29 @@
+# Printing a multi-line string using triple quotes.
+# The text 'hello' will be on the first line and 'world' on the second line, indented.
+print('''hello 
+      world''')
 
+# Assigning the integer 123 to the variable 'number'.
+number = 123
+
+# Converting the integer 'number' to a string and then printing it.
+# Output will be '123' as a string.
+print(str(number))
+
+# Printing the integer value of 'number' directly.
+# Output will be 123 as an integer.
+print(number)
+
+# Assigning the string 'world' to the variable 'stringname'.
+stringname = 'world'
+
+# Assigning the string 'world1' to the variable 'stringname1'.
+stringname1 = 'world1'
+
+# Printing the second character ('o') of the string stored in 'stringname'.
+# In Python, string indexing starts at 0.
+print(stringname[1])
+
+# Concatenating 'stringname' and 'stringname1' with a space in between and printing.
+# Output will be 'world world1'.
+print(stringname + " " + stringname1)
