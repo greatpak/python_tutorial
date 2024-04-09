44e1ed23de168a7245a38b34e419887c51f48b5a
Muhammad Qaiser Majeed <mqaisermajeed@gmail.com>
2024-04-09 10:57:56 +0500

    Update String_Slicing.py

--- String_Slicing.py
+++ String_Slicing.py
@@ -4,3 +4,10 @@ print(my_string[0:6])
 print(my_string[0:])
 print(my_string[2:])
 print(my_string[:2])
+
+my_string1="I Like Python and %s" % "Javascript"
+print(my_string1)
+
+variable="Java"
+my_string2="I like Python and %s" % variable
+print(my_string2)
